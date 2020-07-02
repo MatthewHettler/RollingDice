71c1b0c82c6b71424422fb3cf4e355f45d435812
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-07-02 09:59:56 -0500

    created random number variable.

--- src/RollingDice.java
+++ src/RollingDice.java
@@ -1,6 +1,12 @@
+import java.util.Random;
+
 public class RollingDice {
 
     public static void main(String[] args) {
+
+        Random coolNumberBro = new Random();
+        coolNumberBro.nextInt();
+
         System.out.println("You rolled a: 6");
     } 
 }
