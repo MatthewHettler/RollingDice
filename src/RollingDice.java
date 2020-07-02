c8b49b4bb926304ba12f23745739b8a941d2c531
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-07-02 10:04:09 -0500

    Fixed issue with 0 showing up on roll.

--- src/RollingDice.java
+++ src/RollingDice.java
@@ -5,7 +5,7 @@ public class RollingDice {
     public static void main(String[] args) {
 
         Random coolNumberBro = new Random();
-        int x = coolNumberBro.nextInt(6);
+        int x = coolNumberBro.nextInt(6) + 1;
 
         System.out.println("You rolled a: " + x);
     } 
